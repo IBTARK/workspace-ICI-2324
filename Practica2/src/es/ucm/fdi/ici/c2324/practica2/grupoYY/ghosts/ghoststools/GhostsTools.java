b227cb2a17aa371a52e3119c7cbcaa57cfa58b48
ElPayip <davidperomingo@gmail.com>
2023-10-20 21:58:33 +0200

    tools bien ubicados

--- Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/ghosts/ghoststools/GhostsTools.java
+++
@@ -1,5 +0,0 @@
-package es.ucm.fdi.ici.c2324.practica2.grupoYY.ghosts.ghoststools;
-
-public class GhostsTools {
-
-}
--- Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/mspacman/mspacmantools/MsPacManTools.java
+++
@@ -1,5 +0,0 @@
-package es.ucm.fdi.ici.c2324.practica2.grupoYY.mspacman.mspacmantools;
-
-public class MsPacManTools {
-
-}
