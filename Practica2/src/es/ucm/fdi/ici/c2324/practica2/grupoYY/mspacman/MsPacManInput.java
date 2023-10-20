112055ab833c42ee6ec09c98aa0644d54be0c4c5
ElPayip <davidperomingo@gmail.com>
2023-10-20 23:33:35 +0200

    dangerLevel y levelUp

--- Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/mspacman/MsPacManInput.java
+++ Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/mspacman/MsPacManInput.java
@@ -1,19 +1,40 @@
 package es.ucm.fdi.ici.c2324.practica2.grupoYY.mspacman;
 
 import es.ucm.fdi.ici.Input;
+import pacman.game.Constants.GHOST;
 import pacman.game.Game;
 
 public class MsPacManInput extends Input {
+	
+	// Thresholds
+	private static final int TH_CHASING_GHOST = 50; 
+	
+	private int dangerLevel = 0;
+	private boolean levelUp = false;
 
 	public MsPacManInput(Game game) {
 		super(game);
-		
 	}
 
 	@Override
 	public void parseInput() {
-		// does nothing.
-
+		for (GHOST g : GHOST.values()) {
+			if (TH_CHASING_GHOST > game.getShortestPathDistance(game.getGhostCurrentNodeIndex(g), 
+											 					game.getPacmanCurrentNodeIndex(), 
+											 					game.getGhostLastMoveMade(g)))
+				dangerLevel++;
+		}
+		
+		levelUp = game.getCurrentLevelTime() == 0;
+		
+		
 	}
-
+	
+	public int dangerLevel() {
+		return dangerLevel;
+	}
+	
+	public boolean levelUp() {
+		return levelUp;
+	}
 }
