7d53c1e0917e05b576192a0215de0d46f77a0cbd
ElPayip <davidperomingo@gmail.com>
2023-10-22 14:41:57 +0200

    pacmanFarFromPPill y getNearestChasing

--- Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/ghosts/GhostsInput.java
+++ Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/ghosts/GhostsInput.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import es.ucm.fdi.ici.Input;
+import es.ucm.fdi.ici.c2324.practica2.grupoYY.tools.GhostsTools;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Game;
 
 public class GhostsInput extends Input {
+	
+	//Thresholds
+	private static final int TH_PACMAN_PPILL = 30;
 
 	private Map<GHOST, Boolean> edible = new HashMap<>();
+	private Map<GHOST, GHOST> nearestChasing = new HashMap<>();
 	private double minPacmanDistancePPill;
 	
 	public GhostsInput(Game game) {
@@ -19,25 +24,35 @@ public class GhostsInput extends Input {
 
 	@Override
 	public void parseInput() {
-		for (GHOST g : GHOST.values())
+		for (GHOST g : GHOST.values()) {
 			edible.put(g, game.isGhostEdible(g));
+			nearestChasing.put(g, GhostsTools.getNearestChasing(game, g));
+		}
 	
 		int pacman = game.getPacmanCurrentNodeIndex();
+		
 		this.minPacmanDistancePPill = Double.MAX_VALUE;
 		for(int ppill: game.getPowerPillIndices()) {
 			double distance = game.getDistance(pacman, ppill, DM.PATH);
 			this.minPacmanDistancePPill = Math.min(distance, this.minPacmanDistancePPill);
 		}
+		
+		
 	}
 
 	public boolean edible(GHOST g) {
 		return edible.get(g);
 	}
+	
 	public double getMinPacmanDistancePPill() {
 		return minPacmanDistancePPill;
 	}
 
-
+	public boolean msPacManFarFromPPill() {
+		return minPacmanDistancePPill > TH_PACMAN_PPILL;
+	}
 	
-	
+	public GHOST getNearestChasing(GHOST g) {
+		return nearestChasing.get(g);
+	}
 }
--- Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/tools/GhostsTools.java
+++ Practica2/src/es/ucm/fdi/ici/c2324/practica2/grupoYY/tools/GhostsTools.java
@@ -1,5 +1,22 @@
 package es.ucm.fdi.ici.c2324.practica2.grupoYY.tools;
 
+import pacman.game.Constants.GHOST;
+import pacman.game.Game;
+
 public class GhostsTools {
 
+	public static GHOST getNearestChasing(Game game, GHOST ghost) {
+		GHOST nearest = null;
+		int minDist = Integer.MAX_VALUE;
+		for (GHOST g : GHOST.values()) {
+			int dist = game.getShortestPathDistance(game.getGhostCurrentNodeIndex(ghost), 
+													game.getGhostCurrentNodeIndex(g), 
+													game.getGhostLastMoveMade(ghost));
+			if (!g.equals(ghost) && minDist > dist) {
+				minDist = dist;
+				nearest = g;
+			}
+		}
+		return nearest;
+	}
 }
