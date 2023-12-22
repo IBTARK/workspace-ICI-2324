090e88cf3d82408dc91b7cde5ee0ec54817da2d1
Yikch <yikchen@ucm.es>
2023-12-22 18:06:56 +0100

    GhostInput y pequeños arreglos

--- Practica5/src/es/ucm/fdi/ici/c2324/practica5/grupo01/ghosts/actions/RunAwayToChasing.java
+++ Practica5/src/es/ucm/fdi/ici/c2324/practica5/grupo01/ghosts/actions/RunAwayToChasing.java
@@ -2,6 +2,7 @@ package es.ucm.fdi.ici.c2324.practica5.grupo01.ghosts.actions;
 
 import es.ucm.fdi.ici.Action;
 import es.ucm.fdi.ici.c2324.practica5.grupo01.ghosts.GhostFuzzyData;
+import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
@@ -19,13 +20,36 @@ public class RunAwayToChasing implements Action {
 	@Override
 	public String getActionId() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Run Away to Chasing";
 	}
 
 	@Override
 	public MOVE execute(Game game) {
-		// TODO Auto-generated method stub
-		return null;
+		MOVE nextMove = MOVE.NEUTRAL;
+		if (game.doesGhostRequireAction(ghost))        //if it requires an action
+        {
+			GHOST nearestChasing = null;
+			int pos = game.getGhostCurrentNodeIndex(ghost);
+			int dist = Integer.MAX_VALUE;
+			int aux;
+			for(GHOST g: GHOST.values()) {
+				if(game.getGhostLairTime(g) <= 0 && !game.isGhostEdible(g)) {
+					aux = game.getShortestPathDistance(pos, game.getGhostCurrentNodeIndex(g), game.getGhostLastMoveMade(ghost));
+					if(aux < dist) {
+						dist = aux;
+						nearestChasing = g;
+					}
+				}
+			}
+			if(nearestChasing != null) {
+				nextMove = game.getApproximateNextMoveTowardsTarget(
+						game.getGhostCurrentNodeIndex(ghost), 
+						game.getGhostCurrentNodeIndex(nearestChasing), 
+						game.getGhostLastMoveMade(ghost), 
+						DM.PATH);
+			}
+        }
+		return nextMove;	
 	}
 
 }
